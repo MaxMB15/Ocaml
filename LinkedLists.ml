a42e68a308f0a58939182b2ffde8a0112fda0c84
Max <MaxMB15@gmail.com>
2017-10-19 11:07:10 -0400

    added a datatype of rlist(linked list) and an append func

--- LinkedLists.ml
+++ LinkedLists.ml
@@ -2,3 +2,15 @@
 
 let test = 4
 
+
+type 'a rlist = Empty | Rcons of 'a * ('a rlist) ref
+
+                               
+
+let rec rapp' r1 r2 = match r1 with
+  | v, Empty -> v, r2
+  | v, l -> v, (rapp' l r2)
+
+let rec rapp r1 r2 = match r1 with
+  | {contents = Empty} -> r1 := !r2
+  | {contents = RCons (x, xs)} -> rapp xs r2
