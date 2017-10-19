5ce1f4df7eb14eebd8e14bc578483f9c853633f4
Max <MaxMB15@gmail.com>
2017-10-19 11:53:21 -0400

    added a counter object, my first object created in Ocaml

--- LinkedLists.ml
+++ LinkedLists.ml
@@ -7,10 +7,26 @@ type 'a rlist = Empty | Rcons of 'a * ('a rlist) ref
 
                                
 
-let rec rapp' r1 r2 = match r1 with
+(*let rec rapp' r1 r2 = match r1 with
   | v, Empty -> v, r2
-  | v, l -> v, (rapp' l r2)
+  | v, l -> v, (rapp' l r2)*)
 
 let rec rapp r1 r2 = match r1 with
   | {contents = Empty} -> r1 := !r2
-  | {contents = RCons (x, xs)} -> rapp xs r2
+  | {contents = Rcons (x, xs)} -> rapp xs r2
+
+
+type counter_obj = {tick: unit -> int ; reset : unit -> unit}
+                 
+let makeCounter () =                               
+  let counter = ref 0 in
+    {tick = (fun () -> (counter := !counter + 1; !counter)) ;
+    reset = (fun () -> counter := 0) }
+
+
+type counter_obj2 = {tick: unit -> int ; reset : unit -> int}
+                 
+let makeCounter2 () =                               
+  let counter = ref 0 in
+    {tick = (fun () -> (counter := !counter + 1 ; !counter)) ;
+    reset = (fun () -> counter := 0 ; !counter) }
